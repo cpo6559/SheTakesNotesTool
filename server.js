8ef48699ca1cd18cf696ed91a4939d67e46b2479
CFM <cpierrelouis37@gmail.com>
2022-01-22 19:05:23 -0600

    CSS finally shows up

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const path = require('path');
+const fs = require("fs");
+
 
 const app = express();
 const PORT = 5500;
 
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
 
-//Sends to the homepage if a pathing issue exists
+
+// index.html --> homepage 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-// Setup listener
+// Listner
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
 
-//Sends notes to the notes.html file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes.html'))
-}); 
 
+// notes --> notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
