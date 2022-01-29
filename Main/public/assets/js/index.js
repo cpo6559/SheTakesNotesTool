95ac201c8504993d6485a5aa10d7bbcd2caaad3c
CFM <cpierrelouis37@gmail.com>
2022-01-29 10:49:57 -0600

    organizing aligned with professor instructions

--- Main/public/assets/js/index.js
+++ Main/public/assets/js/index.js
@@ -26,7 +26,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('/public/routes/api/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -34,7 +34,7 @@ const getNotes = () =>
   });
 
 const saveNote = (note) =>
-  fetch('/api/notes', {
+  fetch('/public/routes/api/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -43,7 +43,7 @@ const saveNote = (note) =>
   });
 
 const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
+  fetch(`/public/routes/api/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
---
+++ Main/routes/notes.js
@@ -0,0 +1,33 @@
+const route = require('express').route();
+
+const store = require('../db/notes.json');
+
+// requesting the existing notes
+
+route.get('../db/notes.json', (req, res) => {
+    store
+        .getNotes()
+        .then(notes => {
+            res.json(notes)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+// posting note function route 
+
+route.post('../db/notes.json', (req, res) => {
+    console.log(req.body)
+    store
+        .addNote(req.body)
+        .then(note => {
+            res.json(note)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+
+module.exports = route;
